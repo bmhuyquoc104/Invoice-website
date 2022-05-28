73f8ae7039061d70adbe518b7705a9d8178c7824
Huy ne <bmhuyquoc104@gmail.com>
2022-05-28 10:57:58 +0700

    2. Add and import  required images for client

---
+++ client/src/custom.d.ts
@@ -0,0 +1,10 @@
+// Create a file named custom.d.ts with the following content:
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: any;
+  export default content;
+}
---
+++ client/src/public/imageResources.ts
@@ -0,0 +1,30 @@
+import React from "react";
+import Logo from "./images/logo.svg";
+import Empty from "./images/illustration-empty.svg";
+import Sun from "./images/icon-sun.svg";
+import Plus from "./images/icon-plus.svg";
+import Moon from "./images/icon-moon.svg";
+import Delete from "./images/icon-delete.svg";
+import Check from "./images/icon-check.svg";
+import Calendar from "./images/icon-calendar.svg";
+import ArrowRight from "./images/icon-arrow-right.svg";
+import ArrowLeft from "./images/icon-arrow-left.svg";
+import ArrowDown from "./images/icon-arrow-down.svg";
+import ExternalLink from "./images/icon-external-link.svg";
+import Avatar from "./images/avatar.png";
+
+export const imageResource = {
+  Logo,
+  Empty,
+  Sun,
+  Plus,
+  Moon,
+  ExternalLink,
+  Delete,
+  Check,
+  Calendar,
+  ArrowRight,
+  ArrowLeft,
+  Avatar,
+  ArrowDown,
+};
