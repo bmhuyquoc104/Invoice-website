53be8457f6d2742fffac526cf3cb65ad48e8be32
Huy ne <bmhuyquoc104@gmail.com>
2022-05-28 11:04:44 +0700

    3. Test Deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { imageResource } from "./public/imageResources";
 function App() {
   return <div className="App">
     <img src={imageResource.Plus} alt="Plus" />
+    <h3>Test deploy</h3>
   </div>;
 }
 
