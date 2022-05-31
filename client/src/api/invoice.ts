d9c0f504147359708e82dd9f9c91f56abf6e4db8
Huy Nè <bmhuyquoc104@gmail.com>
2022-05-31 21:34:48 +0700

    16. Add crud function using axios

---
+++ client/src/api/invoice.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://bmhuyquoc104-invoice-app.herokuapp.com/",
+});
+
+type SenderAddress = {
+  street: string;
+  city: string;
+  postCode: string;
+  country: string;
+};
+
+type ClientAddress = {
+  street?: string;
+  city?: string;
+  postCode?: string;
+  country?: string;
+};
+
+type Item = {
+  name: string;
+  quantity: number;
+  price: number;
+  total: number;
+};
+
+export type Invoice = {
+  id: string;
+  createdAt: string;
+  paymentDue: string;
+  description: string;
+  paymentTerms: number;
+  clientName: string;
+  clientEmail: string;
+  status: string;
+  clientAddress: ClientAddress;
+  senderAddress: SenderAddress;
+  total: number;
+  items: Item[];
+};
+// Function to get all invoices
+const getAllInvoices = () => api.get("/invoices");
+
+// Function to get one invoice
+const getInvoice = (id: string) => api.get(`/invoices/${id}`);
+
+// Function to add new invoice
+const addInvoice = (newInvoice: Invoice) => api.post(`/invoices`, newInvoice);
+
+// Function to edit invoice
+const editInvoice = (id: string, newInvoice: Invoice) =>
+  api.put(`/invoice/${id}`, newInvoice);
+
+// Function to delete invoice
+const deleteInvoice = (id: string) => api.delete(`/invoice/${id}`);
+
+// Export functions 
+export { getAllInvoices, getInvoice, addInvoice, editInvoice, deleteInvoice };
